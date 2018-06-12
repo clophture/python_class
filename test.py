e4f578abe9ecdef4fba620094686ae758712713f
clophture <bruce.cozine@gmail.com>
2018-06-12 14:46:18 -0400

    class day2

--- test.py
+++ test.py
@@ -1,9 +1,26 @@
 #!/usr/bin//env python3
-gender = "male"
+gender = "MALE"
 
 
 import json
 with open('apft_chart.json', 'r') as f:
     chart_dict = json.load(f)
 
-print(chart_dict[gender]['pushups']['17_21']['70'])
+
+
+
+def inputPushup(message):
+    while True:
+        try:
+            userInput = int(input(message))
+        except ValueError:
+            print("Not an integer! Try again.")
+            continue
+        else:
+            return userInput
+            break
+
+pushupRAW = str(inputPushup("How many pushups did you do? \n"))
+
+
+print(chart_dict[gender]['run']['17_21'][pushupRAW])
